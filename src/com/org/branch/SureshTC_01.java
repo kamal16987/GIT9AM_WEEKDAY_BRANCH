37084c7adddc4e7e005ba3bce955a0a2d422ba94
suresh1806 <suresh.csrao99@gmail.com>
2019-03-19 14:22:55 +0530

    updated Suresh_01

---
+++ src/com/org/branch/SureshTC_01.java
@@ -0,0 +1,15 @@
+package com.org.branch;
+
+public class SureshTC_01 {
+
+	public static void main(String[] args) {
+		
+int fact=1;
+for (int i = 1; i <=5; i++) {
+	fact=fact*i;
+	
+}
+System.out.println(fact);
+	}
+
+}
