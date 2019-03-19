069830d5f722e444dbdee210c5aa353fbe391b48
Amzeth <amzeth1992@users.noreply.github.com>
2019-03-19 09:10:40 +0530

    Test Case created

---
+++ src/com/org/branch/Santhosh_01.java
@@ -0,0 +1,12 @@
+package com.org.branch;
+
+public class Santhosh_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Test Case created");
+
+	}
+
+}
