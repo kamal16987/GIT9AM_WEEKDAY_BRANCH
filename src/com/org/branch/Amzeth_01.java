1fd6a5b14acf6052b1b09039d018ef376a52e8bb
Amzeth <amzeth1992@users.noreply.github.com>
2019-03-19 08:50:53 +0530

    added a test case

---
+++ src/com/org/branch/Amzeth_01.java
@@ -0,0 +1,11 @@
+package com.org.branch;
+
+public class Amzeth_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Created a test case");
+
+	}
+
+}
