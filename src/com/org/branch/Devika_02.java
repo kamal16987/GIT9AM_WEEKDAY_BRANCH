7cfead3457fdf219901eee965ff4738d8e51f6e6
vetrikrrish <vetrikrrish@gmail.com>
2019-03-19 12:31:35 +0530

    its me

--- src/com/org/branch/Devika_02.java
+++ src/com/org/branch/Devika_02.java
@@ -11,3 +11,4 @@ public class Devika_02 {
 	}
 
 }
+ 
---
+++ src/com/org/branch/praveenn.java
@@ -0,0 +1,13 @@
+package com.org.branch;
+
+public class praveenn {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s ="praveen";
+		System.out.println(s.length());
+		
+
+	}
+
+}
