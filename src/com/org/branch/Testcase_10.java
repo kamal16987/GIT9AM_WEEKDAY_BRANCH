2b792f5e9f75bd6adc90412eef595548c54a1375
LakshmiShiva <lakshmiv629@gmail.com>
2019-04-29 10:07:09 +0530

    Completed testcase 10

--- src/com/org/branch/Testcase_10.java
+++ src/com/org/branch/Testcase_10.java
@@ -5,10 +5,5 @@ public class Testcase_10 {
 	public static void main(String args[]) {
 		
 		System.out.print("Done with my testcase 10");
-		System.out.print("Done with my testcase 10");
-		System.out.print("Done with my testcase 10");
-		System.out.print("Done with my testcase 10");
-		System.out.print("Done with my testcase 10");
-		
 	}
 }
