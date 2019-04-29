3cdf2bfbd0ca2af4790cc03488aae889c494b18f
LakshmiShiva <lakshmiv629@gmail.com>
2019-04-29 09:43:46 +0530

    Added a class called Monday

---
+++ src/com/org/branch/Monday.java
@@ -0,0 +1,10 @@
+package com.org.branch;
+
+public class Monday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Created this class for Monday");
+	}
+
+}
